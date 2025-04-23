cfe084b70899ab7e841e970816617ed233e777f9
Refanz <refandasuryasaputra@gmail.com>
2025-04-23 23:54:31 +0700

    finish step text preprocessing

--- playstore-scrappping.py
+++ playstore-scrappping.py
@@ -1,17 +1,18 @@
-from google_play_scraper import app, reviews, Sort, reviews_all
 import csv
+
+from google_play_scraper import Sort, reviews_all
 
 scraper_view = reviews_all(
     'com.keplerians.evilnun',
     lang='id',
     country='id',
     sort=Sort.MOST_RELEVANT,
-    count=10000
+    count=12000
 )
 
 with open('evil_nun_reviews.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(['Review'])
-    for review in scraper_view[:10000]:
+    for review in scraper_view[:12000]:
         writer.writerow([review['content']])
 
